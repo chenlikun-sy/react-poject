d37d5c92f588608e7d650b9433802122444624d1
chenlikun <chenlikun@boco.com.cn>
2020-06-20 17:02:26 +0800

    完整框架搭建  暂时未使用antd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+import React from 'react'
+import ReactDom from 'react-dom'
+import Router from './router'
+class App extends React.Component {
+    render(){
+    }
+}
+
+ReactDom.render(<Router/>,document.getElementById("app"))
